65e85e271ee0cb60516b130af2eac2e5f8a83d97
Boris-Morris <boris.morris.pro@gmail.com>
2024-10-16 18:04:59 -0500

    Project Starter minor change

--- app/src/main/java/com/example/campusthrifts/MainActivity.kt
+++ app/src/main/java/com/example/campusthrifts/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.campusthrifts
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+//import androidx.compose.ui.semantics.text
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userEmailTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        userEmailTextView = findViewById(R.id.userEmailTextView)
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            userEmailTextView.text = currentUser.email
+        } else {
+            userEmailTextView.text = "No user logged in"
         }
+
     }
 }
