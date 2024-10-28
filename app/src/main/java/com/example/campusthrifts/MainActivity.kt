4621e17aa2eb3064325874ccae816bd207642f12
Boris-Morris <boris.morris.pro@gmail.com>
2024-10-28 00:50:34 -0500

    Create foundation MainActivity

--- app/src/main/java/com/example/campusthrifts/MainActivity.kt
+++ app/src/main/java/com/example/campusthrifts/MainActivity.kt
@@ -1,39 +1,84 @@
 package com.example.campusthrifts
 
-import android.content.Intent
+//import androidx.compose.ui.semantics.text
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.activity.OnBackPressedDispatcher
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-//import androidx.compose.ui.semantics.text
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.auth.FirebaseAuth
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.campusthrifts.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var userEmailTextView: TextView
-    private lateinit var btnLogout: Button
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        userEmailTextView = findViewById(R.id.userEmailTextView)
+        setSupportActionBar(binding.toolbar)
 
-        val currentUser = FirebaseAuth.getInstance().currentUser
-        if (currentUser != null) {
-            userEmailTextView.text = currentUser.email
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_search -> openFragment(SearchFragment())
+                R.id.bottom_cart -> openFragment(CartFragment())
+                R.id.bottom_profile -> openFragment(ProfileFragment())
+                R.id.bottom_chat -> openFragment(ChatFragment())
+            }
+            true
+
+        }
+
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+
+
+
+
+
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> openFragment(HomeFragment())
+            R.id.nav_profile -> openFragment(ProfileFragment())
+            R.id.nav_dashboard -> openFragment(DashboardFragment())
+            R.id.nav_settings -> openFragment(SettingsFragment())
+            R.id.nav_about -> openFragment(AboutFragment())
+            R.id.nav_logout -> Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
         } else {
-            userEmailTextView.text = "No user logged in"
+            super.onBackPressed()
         }
-        btnLogout = findViewById(R.id.logoutButton)
-        btnLogout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
